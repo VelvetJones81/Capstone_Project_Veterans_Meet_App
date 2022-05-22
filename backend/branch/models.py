433b3da9cd957c0e532704dc39b3c0d8650d48ff
Anthony J Dye <dye0118@gmail.com>
2022-05-22 18:57:07 -0500

    Deleted Everything That I Had Worked On

--- backend/branch/models.py
+++
@@ -1,8 +0,0 @@
-from audioop import maxpp
-from django.db import models
-
-# Create your models here.
-
-class Branch():
-    branch = models.CharField(max_length=10)
-    rank = models.CharField(max_length=50)
--- backend/vets_meet_user/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VetsMeetUserConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vets_meet_user'
